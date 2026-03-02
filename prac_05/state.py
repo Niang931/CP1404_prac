9d8819397eaacce3fce7672e64e93e5033636842
Niang931 <kytnlmw1226@gmail.com>
2026-03-02 10:12:21 +0800

    Write a loop to line up all the states in neat format

--- prac_05/state.py
+++ prac_05/state.py
@@ -17,4 +17,7 @@ while state_code != "":
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
 
+code_width = max(len(code) for code in CODE_TO_NAME.keys())
+for code,name in CODE_TO_NAME.items():
 
+    print(f"{code:{code_width}} is {name}")
